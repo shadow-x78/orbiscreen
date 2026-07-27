b0564ab97b03c51051ef5834f6f698706157e508
SHADOW-x78 <107577376+shadow-x78@users.noreply.github.com>
2026-07-27 17:28:32 +0300

    orbiscreen | v0.10.2 | android: build: add androidx.core:core-splashscreen dependency to fix resource linking

--- clients/android/app/build.gradle.kts
+++ clients/android/app/build.gradle.kts
@@ -87,6 +87,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 
     // Compose BOM
     implementation(platform("androidx.compose:compose-bom:2024.09.03"))
